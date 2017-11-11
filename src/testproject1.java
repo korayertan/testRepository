485530ed2c51a16d8cb3b8619cd5d83468cf2db0
korayertan <macbookpro@2015s-MacBook-Pro.local>
2017-11-11 10:03:55 -0500

    my second test

---
+++ src/testproject1.java
@@ -0,0 +1,26 @@
+public class testproject1 {
+
+
+        ForWhileDoLoopExample() {
+        }
+
+        public static void main(String[] args) {
+            int j;
+            for(j = 0; j < 10; ++j) {
+                System.out.println(j);
+            }
+
+            for(j = 0; j < 10; ++j) {
+                System.out.println(j);
+            }
+
+            int k = 0;
+
+            do {
+                System.out.println(k);
+                ++k;
+            } while(k < 4);
+
+        }
+    }
+
